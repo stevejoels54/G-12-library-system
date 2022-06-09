7793cf745194c6723fcb68c4f8f27616eabefc1b
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2022-06-09 20:07:00 +0300

    app header and body

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import homePage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homePage, name='home'),
 ]
