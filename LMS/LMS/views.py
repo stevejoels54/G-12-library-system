5c7194448a49e90da0af69d208eea9a2f1b9d1f3
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2022-07-04 16:08:57 +0300

    new project organisation

--- LMS/LMS/views.py
+++ LMS/LMS/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login
 
 
 def homeView(request):
-    return render(request, "homePage.html", {})
+    return render(request, "home_page.html", {})
 
 
 def loginView(request):
@@ -13,13 +13,12 @@ def loginView(request):
         password = request.POST.get("password")
         context["username"] = username
         context["password"] = password
-        print(username, password)
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request, "homePage.html", context)
+            return render(request, "home_page.html", context)
         else:
             context["error"] = "Invalid username or password"
-            return render(request, "loginPage.html", context)
+            return render(request, "login_page.html", context)
 
-    return render(request, "loginPage.html", context=context)
+    return render(request, "login_page.html", context=context)
