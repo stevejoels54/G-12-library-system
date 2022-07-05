61da87c4c237acdd21e8ce5f59ad4c581a2602ae
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-07-05 18:02:22 +0300

    Payments models

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -1,29 +1,17 @@
-"""LMS URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from .views import homeView
-from .views import loginView 
+from .views import loginView
 from .views import dashboardView
 from .views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', homeView, name='home'),
+
     path('login/', loginView, name='login'),
-    path('dashboard/', dashboardView, name='dashboard'),
     path('signup/', signup, name="signup"),
+
+    path('home/', homeView, name='home'),
+    path('dashboard/', dashboardView, name='dashboard'),
+    path('user-profile/<str:pk>/', dashboardView, name='dashboard'),
 ]
--- LMS/library_books/admin.py
+++ LMS/library_books/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Books
+from .models import Books, Payments
 
 admin.site.register(Books)
+admin.site.register(Payments)
+
