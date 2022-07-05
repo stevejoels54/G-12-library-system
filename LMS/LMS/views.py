043236be8a451ef8f6cdb180133ac23b8b016442
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-07-05 18:25:03 +0300

    Possible custom user fix

--- LMS/LMS/views.py
+++ LMS/LMS/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from customuser.models import CustomUser
 from django.contrib.auth import authenticate, login
 
 
 def homeView(request):
     return render(request, "home_page.html", {})
+
 
 def dashboardView(request):
     return render(request, "dashboard.html", {})
@@ -27,6 +28,7 @@ def loginView(request):
 
     return render(request, "login_page.html", context=context)
 
+
 def signup(request):
     if request.method == "POST":
         user_name = request.POST['username']
@@ -35,8 +37,8 @@ def signup(request):
         email = request.POST['email']
         password = request.POST['password']
 
-        new_user = User.objects.create_user(user_name, email, password)
-        new_user.first_name = first_name 
+        new_user = CustomUser.objects.create_user(user_name, email, password)
+        new_user.first_name = first_name
         new_user.last_name = last_name
 
         return redirect('/login/')
