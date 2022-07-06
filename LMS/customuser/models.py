1b0aa9fcbc157ad26979727124fdfc5d5a2804c6
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-07-06 17:11:40 +0300

    Added role to user model

--- LMS/customuser/models.py
+++ LMS/customuser/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
     password = models.CharField(max_length=50)
     date_of_birth = models.DateField(max_length=50, null=True)
     sex = models.CharField(max_length=10, null=True)
+    role = models.CharField(max_length=10, default='Student')
 
     def __str__(self):
         return self.name
-
