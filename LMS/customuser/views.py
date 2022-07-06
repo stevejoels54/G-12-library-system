12590031412f4d5dd9e224edc8e8f14e728b3552
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-07-06 19:20:29 +0300

    Payments view added

--- LMS/customuser/views.py
+++ LMS/customuser/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import CustomUser
+from .models import CustomUser, UserPayment
+from library_books.models import Book
 
 
 @login_required(login_url='login')
@@ -11,8 +12,24 @@ def userProfile(request, pk):
     return render(request, 'customuser/profile_template.html', context)
 
 
-@login_required(login_url='login/')
-def userPayments(request):
-    context = {'user': user}
+@login_required(login_url='login')
+def userPayments(request, pk):
+    user = CustomUser.objects.get(id=pk)
+    details = {}
+    payments = {}
+
+    borrowed_books = Book.objects.filter(status='Borrowed')
+
+    for book in borrowed_books:
+        student = CustomUser.objects.get(id=book.borrower_id)
+        payment = UserPayment.objects.get(status='Pending')
+        payments[student.name] = payment.amount
+        details[student.name] = book
+
+    role = user.role
+    borrowed_book = Book.objects.get(id=user.id)
+
+    context = {'role': role, 'user': user, 'payments': payments,
+               'borrowed_book': borrowed_book, 'details': details}
 
     return render(request, 'customuser/payments_template.html', context)
