48affe34bd42580ea410039f39217f223da6f65f
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-07-06 19:22:54 +0300

    Registered payments in admin panel

--- LMS/customuser/admin.py
+++ LMS/customuser/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, UserPayment
 
 admin.site.register(CustomUser, UserAdmin)
-
+admin.site.register(UserPayment)
