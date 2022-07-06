f530315f51f31cf9ac375ab19e6a60fe3fbcf0fd
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-07-06 17:10:12 +0300

    Added payments view

--- LMS/customuser/views.py
+++ LMS/customuser/views.py
@@ -9,3 +9,10 @@ def userProfile(request, pk):
 
     context = {'user': user}
     return render(request, 'customuser/profile_template.html', context)
+
+
+@login_required(login_url='login/')
+def userPayments(request):
+    context = {'user': user}
+
+    return render(request, 'customuser/payments_template.html', context)
