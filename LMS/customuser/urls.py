d5dedb4ebf8ff5262c5a7153725c592c373a4336
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-07-06 17:08:39 +0300

    Added payments urls to customuser app

--- LMS/customuser/urls.py
+++ LMS/customuser/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('user-profile/<str:pk>/', views.userProfile, name='profile'),
+    path('user-payments/<str:pk>/', views.userPayments, name='payments'),
 ]
