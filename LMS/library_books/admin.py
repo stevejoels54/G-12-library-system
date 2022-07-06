7f4b76457418c4a6b15ab77ba7fa0bd744259f81
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-07-06 19:25:45 +0300

    Fixed registration of book model in admin panel

--- LMS/library_books/admin.py
+++ LMS/library_books/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from .models import Books, Payments
+from .models import Book
 
-admin.site.register(Books)
-admin.site.register(Payments)
-
+admin.site.register(Book)
