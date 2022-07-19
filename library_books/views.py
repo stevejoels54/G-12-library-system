c38bd3581ff8023ec374500733e203fb15d5b4a2
ELEAZAR MISHEAL <eleazarmish@gmail.com>
2022-07-20 01:44:19 +0300

    Fixed dashboard no model error

--- library_books/views.py
+++ library_books/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Book, Request
-from customuser.models import CustomUser
+from customuser.models import CustomUser, UserPayment
 from datetime import datetime
 
 
